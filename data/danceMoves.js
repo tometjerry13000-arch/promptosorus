8d669a205a778257d0141c911fb4b5fcd59bb814
tometjerry13000-arch <tometjerry13000@gmail.com>
2026-03-14 11:44:13 +0100

    Add files via upload

---
+++ data/danceMoves.js
@@ -0,0 +1,214 @@
+// data/danceMoves.js
+const danceMoves = {
+    spain: {
+        part1: [
+            "zapateados légers - frappes de pieds rythmées",
+            "braceos amples - mouvements de bras majestueux",
+            "floreo - rotations des poignets",
+            "jeux de doigts complexes"
+        ],
+        part2: [
+            "zapateados puissants avec accélérations",
+            "braceos encore plus amples et majestueux",
+            "jeux avec les volants de la robe",
+            "tours complets faisant virevolter la robe"
+        ]
+    },// Pour chaque pays, tu peux ajouter des moves "fous"
+spain: {
+    part1: [
+        "zapateados légers",
+        "braceos amples",
+        "floreo - rotations des poignets",
+        "jeux de doigts complexes",
+        // NOUVEAUX MOVES SPECTACULAIRES
+        "cambrure vertigineuse renversée",
+        "tourbillon de volants explosif"
+    ],
+    part2: [
+        "zapateados puissants",
+        "braceos majestueux",
+        "jeux avec les volants",
+        "tours complets",
+        // NOUVEAUX MOVES SPECTACULAIRES
+        "saute avec claquement de talons en l'air",
+        "renversement arrière avec ouverture de robe"
+    ]
+},
+    brazil: {
+        part1: [
+            "roulements de hanches lents",
+            "vibrations d'épaules",
+            "jeux de pieds rapides",
+            "mouvements de bras ondulants"
+        ],
+        part2: [
+            "roulements de hanches ultra-rapides",
+            "vibrations de fessiers",
+            "jeux avec les plumes",
+            "cambrures spectaculaires"
+        ]
+    },
+    morocco: {
+        part1: [
+            "ondulations lentes du bassin",
+            "vibrations d'épaules",
+            "vagues du ventre",
+            "mouvements de bras gracieux"
+        ],
+        part2: [
+            "ondulations profondes",
+            "shimmies rapides",
+            "jeux avec les fentes du caftan",
+            "mouvements de voile"
+        ]
+    },
+    senegal: {
+        part1: [
+            "vibrations d'épaules rapides",
+            "secousses de hanches",
+            "mouvements de tête fiers"
+        ],
+        part2: [
+            "vibrations intenses d'épaules",
+            "secousses de fessiers",
+            "jeux avec les tresses"
+        ]
+    },
+    japan: {
+        part1: [
+            "gestes lents et contrôlés",
+            "angles de mains parfaits",
+            "pas glissés",
+            "jeux de nuque"
+        ],
+        part2: [
+            "mouvements d'éventail",
+            "découverte de la nuque",
+            "jeux avec le kimono",
+            "tours gracieux"
+        ]
+    },
+    india: {
+        part1: [
+            "rotations des poignets",
+            "ondulations de la taille",
+            "inclinaisons de tête",
+            "vibrations d'épaules"
+        ],
+        part2: [
+            "rotations complètes",
+            "jeux avec les volants",
+            "expressions faciales expressives"
+        ]
+    },
+    china: {
+        part1: [
+            "gestes gracieux des mains",
+            "pas glissés",
+            "postures élégantes"
+        ],
+        part2: [
+            "jeux d'éventail",
+            "mouvements de manches longues",
+            "tours gracieux"
+        ]
+    },
+    greece: {
+        part1: [
+            "pas lents et solennels",
+            "bras levés au ciel",
+            "poses de statue"
+        ],
+        part2: [
+            "cercles gracieux",
+            "cambrures de déesse",
+            "drapés flottants"
+        ]
+    },
+    alien: {
+        part1: [
+            "mouvements robotiques lents",
+            "ondulations extraterrestres",
+            "gestes hypnotiques",
+            "poses de découverte d'un nouveau corps"
+        ],
+        part2: [
+            "mouvements fluides comme l'apesanteur",
+            "vibrations cosmiques",
+            "jeux avec la lumière sur la peau métallique",
+            "poses de créature venue d'ailleurs"
+        ]
+    },
+    mars: {
+        part1: [
+            "mouvements de guerrière",
+            "postures de défi",
+            "gestes puissants"
+        ],
+        part2: [
+            "danse de victoire martienne",
+            "mouvements de combat stylisés",
+            "poses de conquérante"
+        ]
+    },
+    venus: {
+        part1: [
+            "gestes gracieux et lents",
+            "ondulations célestes",
+            "poses de déesse"
+        ],
+        part2: [
+            "danse aérienne",
+            "mouvements de voiles",
+            "poses divines"
+        ]
+    },
+    jupiter: {
+        part1: [
+            "mouvements puissants",
+            "postures majestueuses",
+            "gestes de domination"
+        ],
+        part2: [
+            "danse de tempête",
+            "mouvements tourbillonnants",
+            "poses de reine"
+        ]
+    },
+    saturne: {
+        part1: [
+            "gestes flottants",
+            "mouvements circulaires",
+            "poses avec les anneaux"
+        ],
+        part2: [
+            "danse avec les anneaux",
+            "mouvements orbitaux",
+            "poses célestes"
+        ]
+    },
+    pluton: {
+        part1: [
+            "mouvements lents et contrôlés",
+            "gestes de cristal",
+            "poses gelées"
+        ],
+        part2: [
+            "danse comme un cristal qui se forme",
+            "mouvements fracturés",
+            "éclats de lumière"
+        ]
+    },
+    andromede: {
+        part1: [
+            "gestes hypnotiques",
+            "mouvements de nébuleuse",
+            "poses stellaires"
+        ],
+        part2: [
+            "danse galactique",
+            "tourbillon d'étoiles",
+            "poses cosmiques"
+        ]
+    }
+};
